c04fbf7c1281020c98a9de4e695f17de1fd67b34
Matheus Florencio <126944619+theusrodriguesz@users.noreply.github.com>
2023-05-19 16:31:19 -0300

    Update soma.py
    ..

--- soma.py
+++ soma.py
@@ -1,6 +1,1 @@
-# Inicio do projeto (CALCULADORA)
-calculadora = input("CALCULADORA EFICIENTE, Deseja fazer algum calculo? ")
-calculo = input("Qual soma deseja realizar? ")
-
-print(calculadora)
-print(calculo)
+#Primeiros projetos PY
